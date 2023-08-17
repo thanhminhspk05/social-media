d684ed7ddfd8fb53161568879348d959792cabcc
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-17 14:05:07 +0700

    save

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,5 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 3001,
-    open: true,
-  },
-  build: {
-    outDir: './build',
-  },
-  base: './',
-  plugins: [
-    react({
-      tsDecorators: true,
-    }),
-  ],
-});
+  plugins: [react()],
+})

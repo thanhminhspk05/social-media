b3847d1bf2d4b52deb49d7df6313e4c2102c2392
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2023-08-17 13:19:32 +0700

    save

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,18 +1,5 @@
-import styled from '@emotion/styled';
 import { Box } from '@mui/material';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Post from './Post';
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-}));
 
 const Feed = () => {
   return (
